2b4bfd97ea7b92deb051930824bd8b7bce6cc0b8
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-01 02:10:37 +0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomersPage() {
+  return (
+    <div>
+      <h1>This is Customer page</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesPage() {
+  return (
+    <div>
+      <h1>This is Invoices page</h1>
+    </div>
+  );
+}
